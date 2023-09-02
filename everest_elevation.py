c9543605d5300f1faec31f05245b061c7a656b46
HDeBoever <henrideboever@gmail.com>
2023-09-02 18:17:51 +0200

    black all files

--- everest_elevation.py
+++ everest_elevation.py
@@ -1,10 +1,10 @@
 import ee
 
-service_account = 'gee-service-account@gee-experimentation.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, 'private-key.json')
+service_account = "gee-service-account@gee-experimentation.iam.gserviceaccount.com"
+credentials = ee.ServiceAccountCredentials(service_account, "private-key.json")
 ee.Initialize(credentials)
 
-dem = ee.Image('USGS/SRTMGL1_003')
+dem = ee.Image("USGS/SRTMGL1_003")
 xy = ee.Geometry.Point([86.9250, 27.9881])
-elev = dem.sample(xy, 30).first().get('elevation').getInfo()
-print('Mount Everest elevation (m):', elev)
+elev = dem.sample(xy, 30).first().get("elevation").getInfo()
+print("Mount Everest elevation (m):", elev)
--- static_LST_mapping.py
+++ static_LST_mapping.py
@@ -6,27 +6,27 @@ from IPython.display import Image
 from scipy import optimize
 
 
-service_account = 'gee-service-account@gee-experimentation.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, 'private-key.json')
+service_account = "gee-service-account@gee-experimentation.iam.gserviceaccount.com"
+credentials = ee.ServiceAccountCredentials(service_account, "private-key.json")
 ee.Initialize(credentials)
 
 # Import the MODIS land cover collection.
-lc = ee.ImageCollection('MODIS/006/MCD12Q1')
+lc = ee.ImageCollection("MODIS/006/MCD12Q1")
 
 # Import the MODIS land surface temperature collection.
-lst = ee.ImageCollection('MODIS/006/MOD11A1')
+lst = ee.ImageCollection("MODIS/006/MOD11A1")
 
 # Import the USGS ground elevation image.
-elv = ee.Image('USGS/SRTMGL1_003')
+elv = ee.Image("USGS/SRTMGL1_003")
 
 # Initial date of interest (inclusive).
-i_date = '2017-01-01'
+i_date = "2017-01-01"
 
 # Final date of interest (exclusive).
-f_date = '2020-01-01'
+f_date = "2020-01-01"
 
 # Selection of appropriate bands and dates for LST.
-lst = lst.select('LST_Day_1km', 'QC_Day').filterDate(i_date, f_date)
+lst = lst.select("LST_Day_1km", "QC_Day").filterDate(i_date, f_date)
 
 # Define the urban location of interest as a point near Lyon, France.
 u_lon = 4.8148
@@ -46,17 +46,23 @@ roi = u_poi.buffer(1e6)
 lst_img = lst.mean()
 
 # Adjust for scale factor.
-lst_img = lst_img.select('LST_Day_1km').multiply(0.02)
+lst_img = lst_img.select("LST_Day_1km").multiply(0.02)
 
 # Convert Kelvin to Celsius.
-lst_img = lst_img.select('LST_Day_1km').add(-273.15)
+lst_img = lst_img.select("LST_Day_1km").add(-273.15)
 
 # Create a URL to the styled image for a region around France.
-url = lst_img.getThumbUrl({
-    'min': 10, 'max': 30, 'dimensions': 512, 'region': roi,
-    'palette': ['blue', 'yellow', 'orange', 'red']})
+url = lst_img.getThumbUrl(
+    {
+        "min": 10,
+        "max": 30,
+        "dimensions": 512,
+        "region": roi,
+        "palette": ["blue", "yellow", "orange", "red"],
+    }
+)
 print(url)
 
 # Display the thumbnail land surface temperature in France.
-print('\nPlease wait while the thumbnail loads, it may take a moment...')
+print("\nPlease wait while the thumbnail loads, it may take a moment...")
 Image(url=url)

eba92dc2a399cff9b2768df5b0f05dab6d70b7e6
HDeBoever <henrideboever@gmail.com>
2023-10-31 16:12:01 +0100

    optimise topography func, add grids

--- lune_impacts.py
+++ lune_impacts.py
@@ -36,13 +36,12 @@ def apply_mask(dem, mask_matrix, crater_num):
     temp_dem = np.copy(dem)
     temp_dem[temp==0] = np.nan
 
-    # testing for graph
+    # testing by graphing
     # plt.figure()
     # plt.imshow(temp)
     # plt.imshow(temp_dem)
     # plt.colorbar()
     # plt.show()
-
     # print(temp)
     # print(temp_dem)
 
@@ -59,26 +58,22 @@ def get_crater_topography(dem, mask_matrix, direction, crater_dict):
     cols = 8
     rows = ceil(len(crater_dict) / cols)
 
-    if direction == 'WE':        
-        for index, key in enumerate(crater_dict):
-            temp = mask_matrix==index+1
-            temp_dem = np.copy(dem)
-            temp_dem[temp==0] = np.nan
-            profile = np.where(temp_dem == np.nanmin(temp_dem))
-            plt.subplot(rows, cols, index + 1) 
-            plt.plot(temp_dem[profile[0][0]])
+    for index, key in enumerate(crater_dict):
+        temp = mask_matrix==index+1
+        temp_dem = np.copy(dem)
+        temp_dem[temp==0] = np.nan
+        profile_location = np.where(temp_dem == np.nanmin(temp_dem))
+        plt.subplot(rows, cols, index + 1) 
+        if direction == 'WE':
+            plt.plot(temp_dem[profile_location[0][0]])
+        else:
+             plt.plot(temp_dem[:,profile_location[1][0]])
+    if direction == 'WE':         
         plt.title('Transects en Ouest-Est')
-        plt.show()
-    elif direction == 'NS':
-        for index, key in enumerate(crater_dict):
-            temp = mask_matrix==index+1
-            temp_dem = np.copy(dem)
-            temp_dem[temp==0] = np.nan
-            profile = np.where(temp_dem == np.nanmin(temp_dem))
-            plt.subplot(rows, cols, index + 1) 
-            plt.plot(temp_dem[:,profile[1][0]])
-        plt.title('Transects en Nord-Sud')
-        plt.show()
+    else:
+         plt.title('Transects en Nord-Sud')
+    plt.show()
+
     return None
 
 def get_crater_data():
@@ -86,7 +81,7 @@ def get_crater_data():
     terrain = plot_data('lune_impacts.asc', False)
     masque = plot_data('lune_masque.asc', False)
 
-    for i in range(1,int(np.max(masque)+ 1)):
+    for i in range(1,int(np.max(masque) + 1)):
         curr_num, curr_depth, curr_len, curr_num_cells = apply_mask(terrain,masque,i)
         curr_radius = np.sqrt((curr_num_cells*480**2)/np.pi)
         crater_data[curr_num] = curr_depth, curr_len, round(curr_radius,2)
@@ -95,24 +90,28 @@ def get_crater_data():
 
 def plot_crater_data(crater_data):
 
-    depths = []
+    profondeurs = []
     rayons = []
     for k,v in crater_data.items():
-        depths.append(v[0])
+        profondeurs.append(v[0])
         rayons.append(v[2])
 
     plt.scatter(
-        depths, 
+        profondeurs, 
         rayons,
         color='black',
         label='Rayon de cratère'
     )
 
-    p = np.polyfit(depths, np.log(rayons), 1)
+    plt.scatter(profondeurs[16], rayons[16], color='red', label='Max.')
+    plt.scatter(profondeurs[48], rayons[48], color='green', label='Moyen')
+    plt.scatter(profondeurs[96], rayons[96], color='blue', label='Min.')
+
+    p = np.polyfit(profondeurs, np.log(rayons), 1)
     # Convert the polynomial back into an exponential
     a = np.exp(p[1])
     b = p[0]
-    depths_fitted = np.linspace(np.min(depths), np.max(depths), 1111)
+    depths_fitted = np.linspace(np.min(profondeurs), np.max(profondeurs), 1111)
     y_fitted = a * np.exp(b * depths_fitted)
 
     plt.plot(
@@ -129,6 +128,7 @@ def plot_crater_data(crater_data):
     plt.xlabel ('Profondeur de cratère (m)', fontsize=16)
     plt.ylabel ('Rayon du cratère (m) ', fontsize=16)
     plt.title('Rayon de cratère en fonction de la profondeur')
+    plt.grid()
     plt.show()
 
 def analyse_crater_radius(craters, R_planete):
@@ -157,9 +157,10 @@ def analyse_crater_radius(craters, R_planete):
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys())
-    plt.xlabel ('Rayon de cratère (m)', fontsize=16)
+    plt.xlabel ('Rayon du cratère (m)', fontsize=16)
     plt.ylabel ('Rayon du corps impactant (m) ', fontsize=16)
     plt.title('Rayon du corps impactant en fonction du rayon du cratère')
+    plt.grid()
     plt.show()
         
     return None
@@ -179,9 +180,9 @@ def main(argv):
 
     craters = get_crater_data()
     plot_crater_data(craters)
-
-    terrain = plot_data('lune_impacts.asc', True)
-    masque = plot_data('lune_masque.asc', True)
+    # print(craters)
+    terrain = plot_data('lune_impacts.asc', False)
+    masque = plot_data('lune_masque.asc', False)
 
     get_crater_topography(terrain, masque, 'WE', craters)
     get_crater_topography(terrain, masque, 'NS', craters)
